e043fc810304d00c4a5825d43afd6ab8fb5888d8
tschelovek <az18rg@gmail.com>
2024-03-15 03:44:21 +0300

    presentation

--- js/main.js
+++ js/main.js
@@ -1,26 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     /**
-     *
+     * Анимации в разделе section.hero
      */
 
     const heroBgSvg = document.getElementById('svg_bg10');
     const heroText = document.getElementById('hero_content');
+    const parallelogramsContainer = document.querySelector('.hero__img-wrapper_3');
     const textEl1 = heroText.querySelector('.el_1');
     const textEl2 = heroText.querySelector('.el_2');
+    const startTime = 400;
 
     gsap.registerPlugin(TextPlugin);
 
-    setTimeout(() => heroBgSvg.querySelector('.el_l3').classList.add('show'), 200);
-    setTimeout(() => heroBgSvg.querySelector('.el_l2').classList.add('show'), 500);
-    setTimeout(() => heroBgSvg.querySelector('.el_l1').classList.add('show'), 900);
-    setTimeout(() => heroBgSvg.querySelector('.el_10').classList.add('show'), 1300);
-    setTimeout(() => heroBgSvg.querySelector('.el_gt').classList.add('show'), 1500);
-    setTimeout(() => heroText.querySelector('.years').classList.add('show'), 1400);
-    setTimeout(() => gsap.to(textEl1, {duration: 1.5, text: "строим и развиваем"}), 1500);
-    setTimeout(() => gsap.to(textEl2, {duration: 1, text: "вместе с Вами!"}), 2500);
+    setTimeout(() => heroBgSvg.querySelector('.el_l3').classList.add('show'), startTime);
+    setTimeout(() => heroBgSvg.querySelector('.el_l2').classList.add('show'), startTime + 300);
+    setTimeout(() => heroBgSvg.querySelector('.el_l1').classList.add('show'), startTime + 700);
+    setTimeout(() => heroBgSvg.querySelector('.el_10').classList.add('show'), startTime + 1100);
+    setTimeout(() => heroBgSvg.querySelector('.el_gt').classList.add('show'), startTime + 1300);
+    setTimeout(() => heroText.querySelector('.years').classList.add('show'), startTime + 1200);
+    setTimeout(() => gsap.to(textEl1, {duration: 1.5, text: "строим и развиваем"}), startTime + 1300);
+    setTimeout(() => gsap.to(textEl2, {duration: 1, text: "вместе с Вами!"}), startTime + 2300);
+    setTimeout(
+        () => parallelogramsContainer.classList.remove('hide'),
+        startTime + 3000
+    );
 
-
+    /**
+     * Скукоживатель шапки при прокрутке
+     */
+    window.addEventListener('scroll', () => {
+        const header = document.body.querySelector('header');
+        if(window.scrollY > 0) {
+            !header.classList.contains('shrink') ? header.classList.add('shrink') : null;
+            parallelogramsContainer.classList.add('hide');
+        } else {
+            header.classList.remove('shrink');
+            parallelogramsContainer.classList.remove('hide')
+        }
+    })
+    //*На случай перезагрузки страницы проверяем положение и если надо вешаем скукоживатель на шапку
+    if (window.scrollY > 0) document.body.querySelector('header')?.classList.add('shrink')
 
 
     /**
